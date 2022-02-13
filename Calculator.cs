cea02458cfffd98e5c051612bbc629a75c590638
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-02-14 01:37:16 +0500

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator:ICalculator
+    {
+        public int Sum(int first, int second)
+        {
+            return first + second;
+        }
+        public int Substract(int first, int second)
+        {
+            return first - second;
+        }
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System;
+
+Calculator cal = new Calculator();
+Console.WriteLine($"The Sum of 7 and 4 is {cal.Substract(7, 4)}");
+Console.WriteLine($"The Mul of 7 and 4 is { cal.Multiply(7, 4)}");
+Console.WriteLine($"The Sub of 7 and 4 is {cal.Sum(7, 4)}");
